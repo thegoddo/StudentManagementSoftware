7ff01509765cd52cebb7be04fda335d83e1e4c73
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-05-05 23:46:29 +0530

    Initial commit

---
+++ src/main/java/com/project/StudentManagementSoftware/StudentManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.project.StudentManagementSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/StudentManagementSoftware/StudentManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.StudentManagementSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
